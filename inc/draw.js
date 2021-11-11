6be129cf88298f044377f830750aca7778ea3883
bob <bob@efik.cz>
2021-11-11 21:47:23 +0100

    HB refaktoring

--- inc/draw.js
+++ inc/draw.js
@@ -1,59 +1,107 @@
+
+var useckaCount = 0;
 var useckaSerie = [];
-var usecka_count = 0;
 
-function drawUsecka(a, b, color) {
-    useckaSerie[usecka_count] = chart.addLineSeries({
-        color: color,
-        lineStyle: 0,
-        lineWidth: 1,
-        crosshairMarkerVisible: false,
-        lastValueVisible: false,
-        priceLineVisible: false,
-    });
-    useckaSerie[usecka_count].setData([
-        { time: a.t, value: a.p },
-        { time: b.t, value: b.p }
-    ]);
-
-    usecka_count++;
+function drawUsecka (a, b, color)
+{
+  useckaSerie[usecka_count] = chart.addLineSeries({
+    color: color,
+    lineStyle: 0,
+    lineWidth: 1,
+    crosshairMarkerVisible: false,
+    lastValueVisible: false,
+    priceLineVisible: false,
+  });
+  useckaSerie[usecka_count].setData([
+    { time: a.t, value: a.p },
+    { time: b.t, value: b.p }
+  ]);
+  useckaCount++;
 }
 
-function drawUsecka_delete() {
-    $.each( useckaSerie, function( key, val ) {
-        //console.log(val);
-        chart.removeSeries(val);
-    });
-    usecka_count = 0;
-    useckaSerie = [];
-}
-
-
-// HB
+//
 
 var curveSeries = {};
 
-function drawKrivka(data) {
-    //logt(data);
+function drawKrivka (data)
+{
+  points = [];
+  $.each(data.points, function(key, val) {
+    points.push({ time: val.t, value: val.p });
+  });
+  index = "" + data.index;
 
-    points = [];
-    $.each( data.points, function( key, val ) {
-        points.push({ time: val.t, value: val.p });
+  if (curveSeries[index] !== undefined)
+    chart.removeSeries(curveSeries[index]);
+
+  if (points.length > 0)
+  {
+    curveSeries[index] = chart.addLineSeries({
+      color: data.color,
+      lineStyle: 0,
+      lineWidth: 1,
+      crosshairMarkerVisible: false,
+      lastValueVisible: false,
+      priceLineVisible: false,
     });
-    index = "" + data.index;
-  
-    if (curveSeries[index] !== undefined)
-      chart.removeSeries(curveSeries[index]);
+    curveSeries[index].setData(points);
+  }
+}
 
-    if (points.length > 0)
-    {
-        curveSeries[index] = chart.addLineSeries({
-            color: data.color,
-            lineStyle: 0,
-            lineWidth: 1,
-            crosshairMarkerVisible: false,
-            lastValueVisible: false,
-            priceLineVisible: false,
-        });
-        curveSeries[index].setData(points);
-    }
+//
+
+var dotsSeries = {};
+
+function drawDots (data)
+{
+  dots = [];
+  hh = data.hh;
+  $.each(data.points, function(key, val) {
+    //console.log(val);
+    //max_value = Math.max(0, val.p);
+    //heightLimiter = (max_value / data.div) / 2;
+    k = (val.lh == 'H')? -1:1;
+    dots.push({ time: val.t, open: val.p + k*hh, high: val.p + hh, low: val.p - hh, close: val.p - k*hh });
+  });
+  index = "" + data.index;
+
+  if (dotsSeries[index] !== undefined)
+    chart.removeSeries(dotsSeries[index]);
+
+console.log(data.index +':'+ dots.length);
+
+  if (dots.length > 0)
+  {
+    dotsSeries[index] = chart.addCandlestickSeries({
+      upColor: data.c2,
+      downColor: data.c1,
+      borderVisible: false,
+      wickVisible: false,
+      crosshairMarkerVisible: false,
+      lastValueVisible: false,
+      priceLineVisible: false,
+    });
+    dotsSeries[index].setData(dots);
+  }
+}
+
+//
+
+function releaseSeries ()
+{
+  $.each(useckaSerie, function(key, val) {
+    chart.removeSeries(val);
+  });
+  useckaCount = 0;
+  useckaSerie = [];
+  //
+  $.each(dotsSeries, function(key, val) {
+    chart.removeSeries(val);
+  });
+  dotsSeries = {};
+  //
+  $.each(curveSeries, function(key, val) {
+    chart.removeSeries(val);
+  });
+  curveSeries = {};
 }
--- inc/websocket.js
+++ inc/websocket.js
@@ -1,119 +1,84 @@
-function init_websocket() {
-		ws_force_stop = false;
 
-		socket = new WebSocket('wss://ws-cstor.efik.cz');
+var ws_killed = false;
+var ws_socket;
 
-		// Connection opened
-		socket.addEventListener('open', function (event) {
-				send_parms_websocket();
-		});
 
-		// Listen for messages
-		socket.addEventListener('message', function (event) {
-						let server_response = JSON.parse(event.data);
-						//console.log(server_response);
+function ws_init()
+{
+  ws_killed = false;
 
-						if (server_response.t !== undefined) { // 't'
-								//Jen pokud zrovna neprobíhá načítání starších dat
-								if (!pridatStarsiDataDoGrafu_locked && povoleni_k_pridavani_novych_svicek) {
-										//Přidáme novou svíčku
-										updateLastBar(server_response);
-								}
-						} else if (server_response.new_ts !== undefined) {
-								new_ts(server_response.new_ts);
-						} else if (server_response.dots !== undefined) {
-								//Draw DOTS
+  ws_socket = new WebSocket('wss://ws-cstor.efik.cz');
 
-								$.each( server_response.dots.data, function( key, val ) {
+  // Connection opened
+  ws_socket.addEventListener('open', function (event) {
+    ws_send_parms();
+  });
 
-										let nalezeno = false;
-										let i;
-										for (i = 0; i < data_pro_graf.length; i++) {
-												if (data_pro_graf[i].time === val.t) {
-														nalezeno = true;
-												}
-										}
+  // Listen for messages
+  ws_socket.addEventListener('message', function (event) {
+    let server_response = JSON.parse(event.data);
+    //console.log(server_response);
 
-										//Hodíme alert že nebylo nalezeno
-										//if (!nalezeno) alert("Čas DOTu nesouhlasí s časem svíčky. Čas DOTu: " + naformatujTimestamp(val.t));
-
-										dotsSerie[dots_count] = chart.addCandlestickSeries({
-												upColor: val.c,
-												downColor: val.c,
-												borderVisible: false,
-												wickVisible: false,
-												crosshairMarkerVisible: false,
-												lastValueVisible: false,
-												priceLineVisible: false,
-										});
-
-										//Pojdmě spočítat výšku
-										min_value = 0;
-										max_value = 0;
-
-										if (max_value < val.p) max_value = val.p;
-
-										//var heightLimiter = ((max_value - min_value) / ((chart_height/(chart_width/limit))*0.8))/2;
-										heightLimiter = (max_value / server_response.dots.div) / 2;
-
-										dotsSerie[dots_count].setData([{ time: val.t, open: val.p + heightLimiter, high: val.p + heightLimiter, low: val.p - heightLimiter, close: val.p - heightLimiter }]);
-										allDotsSerie.push(dotsSerie[dots_count]);
-										allDotsSerieData.push({ key: dots_count, time: val.t, value: val.p });
-
-										dots_count++;
-								});
-						} else if (server_response.new_parms !== undefined) {
-								//jiné páry a timeframe
-								$('.timeframe_button').removeClass("active");
-								$('#timeframe_' + server_response.new_parms.tf).addClass("active");
-								$("#pairs").val(server_response.new_parms.pair);
-								active_timeline_button = server_response.new_parms.tf;
-								active_timeframe_krok = parseInt(active_timeframe_krok = $('#timeframe_' + server_response.new_parms.tf).attr('data-krok'));
-								pripravitDataProGraf();
-						} else if (server_response.pos_buttons !== undefined) {
-								//pos_buttons
-								$("#pos_buttons").html('');
-								$.each( server_response.pos_buttons, function( key, val ) {
-										$("#pos_buttons").append("<a href='#' class='button' onclick='new_ts("+ val.t +"); return false;'>"+ val.text +"</a>");
-								});
-						} else if (server_response.cl_dots !== undefined) {
-								//cl_dots = smaže všechny dots
-								dots_delete();
-								drawUsecka_delete();
-						} else if (server_response.lines !== undefined) {
-								//draw usecka
-								$.each( server_response.lines, function( key, val ) {
-										drawUsecka(val.A, val.B, val.c);
-								});
-						} else if (server_response.action !== undefined && server_response.action == "restart") {
-								//Nové načtení grafu
-								pripravitDataProGraf();
-						} else if (server_response.curve !== undefined) {
-								//draw krivka
-								drawKrivka(server_response.curve);
-						}
-						else {
-								alert("Neznámé ws: "+ event.data);
-						}
-		});
-
+    if (server_response.t !== undefined) { // 't'
+      //jen pokud zrovna neprobíhá načítání starších dat
+      if (!dataDoGrafu_locked && povoleni_k_pridavani_novych_svicek) {
+        //přidáme novou svíčku
+        updateLastBar(server_response);
+      }
+    } else if (server_response.new_ts !== undefined) {
+      new_ts(server_response.new_ts);
+    } else if (server_response.new_parms !== undefined) {
+      //jiné páry a timeframe
+      $('.timeframe_button').removeClass("active");
+      $('#timeframe_' + server_response.new_parms.tf).addClass("active");
+      $("#pairs").val(server_response.new_parms.pair);
+      active_timeline_button = server_response.new_parms.tf;
+      active_timeframe_krok = parseInt(active_timeframe_krok = $('#timeframe_' + server_response.new_parms.tf).attr('data-krok'));
+      pripravitDataProGraf('4-ws');
+    } else if (server_response.pos_buttons !== undefined) {
+      //pos_buttons
+      $("#pos_buttons").html('');
+      $.each( server_response.pos_buttons, function( key, val ) {
+        $("#pos_buttons").append("<a href='#' class='button' onclick='new_ts("+ val.t +"); return false;'>"+ val.text +"</a>");
+      });
+    } else if (server_response.cl_dots !== undefined) {
+      //cl_dots = smaže všechny vrstvy grafu
+      releaseSeries();
+    } else if (server_response.dots !== undefined) {
+      //draw dots
+      drawDots(server_response.dots);
+    } else if (server_response.lines !== undefined) {
+      //draw usecka
+      $.each( server_response.lines, function( key, val ) {
+        drawUsecka(val.A, val.B, val.c);
+      });
+    } else if (server_response.curve !== undefined) {
+      //draw krivka
+      drawKrivka(server_response.curve);
+    } else if (server_response.action !== undefined && server_response.action == "restart") {
+      //nové načtení grafu
+      pripravitDataProGraf();
+    }
+    else {
+      alert("Neznámé ws: "+ event.data);
+    }
+  });
 }
 
-function send_parms_websocket() {
-		socket.send('{"action": "parms", "pair": "'+ $('#pairs').val() +'", "tf": "'+ active_timeline_button +'", "ts": '+ data_pro_graf[data_pro_graf.length - 1].time +', "n": '+ (data_pro_graf.length - 1) +', "mode": "'+ (test_mode? 't':'r') + '"}');
+function ws_send_parms() {
+  ws_socket.send('{"action": "parms", "pair": "'+ $('#pairs').val() +'", "tf": "'+ active_timeline_button +'", "ts": '+ data_pro_graf[data_pro_graf.length - 1].time +', "n": '+ (data_pro_graf.length - 1) +', "mode": "'+ (test_mode? 't':'r') + '"}');
 }
 
-function close_websocket() {
-		ws_force_stop = true;
-		socket.close();
+function ws_send_click(event) {
+  ws_socket.send('{"action": "click", "ctime": '+ event.time +', "x": '+ event.point.x +', "y": '+ event.point.y +', "w": '+ $("#graf").width() +', "h": '+ $("#graf").height() +'}');
 }
 
-function toggle_candles_websocket(force_off = null) {
-		if (povoleni_k_pridavani_novych_svicek || force_off === true) {
-				povoleni_k_pridavani_novych_svicek = false;
-				$("#button_toggle_candles").removeClass("active");
-		} else {
-				povoleni_k_pridavani_novych_svicek = true;
-				$("#button_toggle_candles").addClass("active");
-		}
+function ws_send_dots() {
+  // !!! limitace na prostrednich 20k zaznamu (podle polohy krize)
+  ws_socket.send('{"action": "graf_rozmezi", "ts": '+ data_pro_graf[0].time +', "te": '+ data_pro_graf[data_pro_graf.length-1].time +', "n": '+ data_pro_graf.length +'}');
+}
+
+function ws_close() {
+  ws_killed = true;
+  ws_socket.close();
 }
